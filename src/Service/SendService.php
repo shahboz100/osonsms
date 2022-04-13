138c09e240488ea8c38fc89563e7b3723a79ddca
user414 <mirbadiev@krasmed.ru>
2022-04-13 14:06:51 +0700

    chore: Промежуточный коммит

--- src/Service/SendService.php
+++ src/Service/SendService.php
@@ -6,7 +6,6 @@ declare(strict_types=1);
 namespace Osonsms\Service;
 
 
-use Osonsms\Exception\InvalidArgumentException;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,12 +16,14 @@ class SendService
     /** @var ContainerInterface $container */
     protected $container;
     protected $url = '';
+    protected $helper = null;
     const METHOD = 'GET';
 
-    public function __construct(ContainerInterface $container)
+    public function __construct(ContainerInterface $container, HelperService $helper)
     {
         $this->container = $container;
-        $this->url = $container->get('osonsms.baseUrl');
+        $this->url = $this->container->getParameter('osonsms.baseUrl');
+        $this->helper = $helper;
     }
 
     /**
@@ -33,17 +34,65 @@ class SendService
      */
     public function send(string $phone, string $msg)
     {
-        if ('' === $this->url) {
-            throw InvalidArgumentException::invalidUrl();
-        }
-
+        $txnId = $this->helper->generateUuid4();
+        $strHash = $this->helper->getHash($txnId);
         try {
             $client = HttpClient::create();
-            $response = $client->request(self::METHOD, $this->url, [/*options*/]);
+            $response = $client->request(self::METHOD, $this->url, [
+                'query' => [
+                    'str_hash' => $strHash,
+                    'txn_id' => $txnId,
+                    'login' => $this->container->getParameter('osonsms.login')
+                ]
+            ]);
 
             switch ($response->getStatusCode()) {
                 case Response::HTTP_CREATED:
                     //TODO: Обработка ответа 201
+                    break;
+                case Response::HTTP_BAD_REQUEST:
+                    //TODO: Обработка ошибки 400
+                    break;
+                case Response::HTTP_INTERNAL_SERVER_ERROR:
+                    //TODO: Обработка ошибки 500
+                    break;
+                default:
+                    //TODO: Обработка ошибки в иных случаях
+            }
+        } catch (TransportExceptionInterface $e) {
+            //TODO: Обработка ошибки вызова
+        }
+    }
+
+    /**
+     * Метод проверяет статус ранее отправленного СМС
+     * @param $txnId
+     */
+    public function checkStatus($txnId)
+    {
+        //TODO: Проверка статуса смс
+    }
+
+    /**
+     * Метод возвращает актуальный баланс на счете
+     */
+    public function getBalance()
+    {
+        $txnId = $this->helper->generateUuid4();
+        $strHash = $this->helper->getHash($txnId);
+        try {
+            $client = HttpClient::create();
+            $response = $client->request(self::METHOD, $this->url, [
+                'query' => [
+                    'str_hash' => $strHash,
+                    'txn_id' => $txnId,
+                    'login' => $this->container->getParameter('osonsms.login')
+                ]
+            ]);
+
+            switch ($response->getStatusCode()) {
+                case Response::HTTP_CREATED:
+                    $result = json_decode($response->getContent());
                     break;
                 case Response::HTTP_BAD_REQUEST:
                     //TODO: Обработка ошибки 400
